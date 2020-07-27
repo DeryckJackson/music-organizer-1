7f26631ae27ef3858d24a837b410839adf88358b
Sara Kane <kane.sarak@gmail.com>
2020-07-27 13:40:32 -0700

    Add database class and connection to the database in startup
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- Music/Controllers/AlbumController.cs
+++ Music/Controllers/AlbumController.cs
@@ -1,10 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using MusicOrganizer.Models;
+using System.Collections.Generic;
 
 namespace MusicOrganizer.Controllers
 {
   public class AlbumController : Controller
   {
-    //Routes go here
+    [HttpGet("/artists/{artistId}/albums/new")]
+    public ActionResult New(int artistId)
+    {
+      Artist artist = Artist.Find(artistId);
+      return View(artist);
+    }
+
+    [HttpGet("/artists/{artistId}/albums/{albumId}")]
+    public ActionResult Show(int artistId, int albumId)
+    {
+      Album album = Album.Find(albumId);
+      Artist artist = Artist.Find(artistId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("album", album);
+      model.Add("artist", artist);
+      return View(model);
+    }
+
+    [HttpPost("/albums/delete")]
+    public ActionResult DeleteAll()
+    {
+      Album.ClearAll();
+      return View();
+    }
   }
 }
