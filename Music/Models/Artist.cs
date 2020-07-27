b452002c81d2a6415a2584132e9f01e04c73e676
Sara Kane <kane.sarak@gmail.com>
2020-07-27 13:52:46 -0700

    Add database query to getAll function of Artist class
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- Music/Models/Artist.cs
+++ Music/Models/Artist.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace MusicOrganizer.Models
 {
@@ -6,34 +7,53 @@ namespace MusicOrganizer.Models
   {
     public string Name { get; }
     public int Id { get; }
-    public List<Album> Albums { get; set; }
-    private static List<Artist> _artistList = new List<Artist> {};
 
     public Artist(string name)
     {
       Name = name;
-      _artistList.Add(this);
-      Id = _artistList.Count;
-      Albums = new List<Album> {};
+    }
+
+    public Artist(string name, int id)
+    {
+      Name = name;
+      Id = id;
     }
 
     public static void ClearAll()
     {
-      _artistList.Clear();
+      
     }
 
     public static List<Artist> GetAll()
     {
-      return _artistList;
+      List<Artist> allArtists = new List<Artist>{};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM artists;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        string name = rdr.GetString(0);
+        int id = rdr.GetInt32(1);
+        Artist newArtist = new Artist(name, id);
+        allArtists.Add(newArtist);
+      }
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+      return allArtists;
     }
     public static Artist Find(int searchId)
     {
-      return _artistList[searchId - 1];
+      return new Artist("name");
     }
 
     public void AddAlbum(Album album)
     {
-      Albums.Add(album);
+      
     }
 
   }
